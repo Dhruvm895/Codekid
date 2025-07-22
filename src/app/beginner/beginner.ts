e0526a0476bea751b4db17a891bb227c208fcb4a
Dhruvm895 <mistrydhruv04@gmail.com>
2025-07-22 23:03:16 +0530

    update

---
+++ src/app/beginner/beginner.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-beginner',
+  imports: [],
+  templateUrl: './beginner.html',
+  styleUrl: './beginner.css'
+})
+export class Beginner {
+
+}
